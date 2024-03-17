2cdc3ba9d2d9c978b737d56812cfa3c19b319409
unaveengoud <unaveengoud.006@gmail.com>
2024-03-17 20:42:15 +0530

    message

--- Jenkins/Infra/us-east-1/infra.tf
+++ Jenkins/Infra/us-east-1/infra.tf
@@ -6,12 +6,14 @@ module "iam_policies_roles" {
 }
 
 module "ecs_cluster" {
+  depends_on=["module.iam_policies_roles"]
   source = "../tf-modules/tf-ECS-EC2"
   aws_vpc = "vpc-0509fde9ba64692ea"
   aws_subnet = ["subnet-0a58f7bb36b69534a","subnet-0ca455ad1f587a812","subnet-0918669d0c271ee73"]
 }
 
 module "eks_cluster" {
+  depends_on=["module.iam_policies_roles"]
   source = "../tf-modules/tf-k8s"
   subnet_ids = ["subnet-0a58f7bb36b69534a","subnet-0ca455ad1f587a812","subnet-0918669d0c271ee73"]
 }
