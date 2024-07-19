814159af8671d8aa484ec9aa17aca30245a016bc
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2024-07-19 21:27:38 +0530

    Update variables.tf

--- Infra/tf-modules/tf-targetgroup/variables.tf
+++ Infra/tf-modules/tf-targetgroup/variables.tf
@@ -5,5 +5,23 @@ type = number
 
 variable "targetgroup_name" {
 type = string
-
 }
+
+variable "VpcId" {
+type = string
+}
+
+
+variable "listenerArn" {
+type = string
+}
+
+
+variable "priorityNumber" {
+type = number
+}
+
+variable "pathName" {
+type = string
+}
+
