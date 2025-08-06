9a68b315906713fbfb2fe48f44a956806f2f9459
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-08-07 00:33:16 +0530

    Update sg.tf

--- Infra/modules/tf-sg/sg.tf
+++ Infra/modules/tf-sg/sg.tf
@@ -1,25 +1,25 @@
 provider "aws" {
-   region = var.aws_region
+   region = "us-east-2"
 }
 
 #Create security group with firewall rules
 resource "aws_security_group" "tcn_security_grp" {
   name        = "tcn Security Group"
   description = "security group for allowing SSH traffic to EC2 instances"
-  vpc_id      = var.vpc_id
+  vpc_id      = "vpc-0d63abbc242241731"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = var.ssh_cidr_block
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = var.ssh_cidr_block
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
