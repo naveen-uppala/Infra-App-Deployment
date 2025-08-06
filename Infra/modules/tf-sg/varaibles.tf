1eeb799f1a5a14997ad1565c99f67dd164836a97
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-08-07 01:32:43 +0530

    Update varaibles.tf

--- Infra/modules/tf-sg/varaibles.tf
+++ Infra/modules/tf-sg/varaibles.tf
@@ -1,6 +1,7 @@
 # SG Variables
 
 variable "vpc_id" {
-  # type       = string
+  type       = string
   description   = "VPC for creating a custom security group"
+  default = "vpc-0d63abbc242241731"
 }
