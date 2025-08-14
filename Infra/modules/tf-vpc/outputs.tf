e1d3e80abfbbcaacbe917144be1922d8f5508a99
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-08-14 23:44:12 +0530

    Create outputs.tf

---
+++ Infra/modules/tf-vpc/outputs.tf
@@ -0,0 +1,27 @@
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "private_subnet_ids" {
+  value = { for k, s in aws_subnet.private : k => s.id }
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.igw.id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat.id
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  value = aws_route_table.private.id
+}
