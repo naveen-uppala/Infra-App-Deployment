568bb27345ce9334192037588913d6291a976bc2
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-08-15 00:00:17 +0530

    Update outputs.tf

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -3,25 +3,25 @@ output "vpc_id" {
 }
 
 output "public_subnet_id" {
-  value = module.vpc.public_subnet_id
+  value = module.tf-vpc.public_subnet_id
 }
 
 output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
+  value = module.tf-vpc.private_subnet_ids
 }
 
 output "internet_gateway_id" {
-  value = module.vpc.internet_gateway_id
+  value = module.tf-vpc.internet_gateway_id
 }
 
 output "nat_gateway_id" {
-  value = module.vpc.nat_gateway_id
+  value = module.tf-vpc.nat_gateway_id
 }
 
 output "public_route_table_id" {
-  value = module.vpc.public_route_table_id
+  value = module.tf-vpc.public_route_table_id
 }
 
 output "private_route_table_id" {
-  value = module.vpc.private_route_table_id
+  value = module.tf-vpc.private_route_table_id
 }
