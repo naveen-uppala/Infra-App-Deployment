272bf3f13175d83ac8e032f7f74292b51dfc23cf
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-08-14 23:43:13 +0530

    Update variables.tf

--- Infra/modules/tf-vpc/variables.tf
+++ Infra/modules/tf-vpc/variables.tf
@@ -1,51 +1,15 @@
-# VPC Variables
+variable "vpc_name" {
+  description = "Name tag for the VPC."
+  type        = string
+}
 
 variable "vpc_cidr" {
-  type          = string
-  description   = "VPC CIDR Block"
+  description = "CIDR for the VPC (e.g., 192.168.0.0/16)."
+  type        = string
 }
 
-variable "web_tier_subnet_1_cidr" {
-  type          = string
-  description   = "Public Subnet 1 CIDR Block"
-}
-
-variable "web_tier_subnet_2_cidr" {
-  type          = string
-  description   = "Public Subnet 2 CIDR Block"
-}
-
-variable "web_tier_subnet_3_cidr" {
-  type          = string
-  description   = "Public Subnet 3 CIDR Block"
-}
-
-variable "app_tier_subnet_1_cidr" {
-  type          = string
-  description   = "Private Subnet 1 CIDR Block"
-}
-
-variable "app_tier_subnet_2_cidr" {
-  type          = string
-  description   = "Private Subnet 2 CIDR Block"
-}
-
-variable "app_tier_subnet_3_cidr" {
-  type          = string
-  description   = "Private Subnet 3 CIDR Block"
-}
-
-variable "data_tier_subnet_1_cidr" {
-  type          = string
-  description   = "Private Subnet 4 CIDR Block"
-}
-
-variable "data_tier_subnet_2_cidr" {
-  type          = string
-  description   = "Private Subnet 5 CIDR Block"
-}
-
-variable "data_tier_subnet_3_cidr" {
-  type          = string
-  description   = "Private Subnet 6 CIDR Block"
+variable "tags" {
+  description = "Extra tags applied to all resources."
+  type        = map(string)
+  default     = {}
 }
