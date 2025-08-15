bb5e1372d61a0f50d415b8065f21eda3df1a6759
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-08-15 09:52:26 +0530

    Update variables.tf

--- Infra/modules/tf-ecr/variables.tf
+++ Infra/modules/tf-ecr/variables.tf
@@ -1,26 +1,4 @@
-variable "aws_region" {
-  description = "Aws Region"
-  type        = string
-}
-
-variable "ecr_name" {
-  description = "The list of ecr names to create"
-  type        = string
-}
-variable "tags" {
-  description = "The key-value maps for tagging"
-  type        = map(string)
-
-}
-
-
-variable "encrypt_type" {
-  description = "Provide type of encryption here"
-  type        = string
-  default     = "KMS"
-}
-
-variable "image_mutability" {
-  description = "The list of ecr names to create"
-  type        = string
+variable "repository_names" {
+  description = "ECR repositories to create"
+  type        = list(string)
 }
