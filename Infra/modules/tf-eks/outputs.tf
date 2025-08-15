72293fe612f23ac5d7ca0aac51db041f4d4989c7
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-08-15 22:44:38 +0530

    Create outputs.tf

---
+++ Infra/modules/tf-eks/outputs.tf
@@ -0,0 +1,12 @@
+output "tf_eks_cluster_name" {
+  value = aws_eks_cluster.tf_eks_cluster.name
+}
+output "tf_eks_cluster_arn" {
+  value = aws_eks_cluster.tf_eks_cluster.arn
+}
+output "tf_eks_cluster_security_group_id" {
+  value = aws_security_group.tf_eks_cluster.id
+}
+output "tf_eks_node_group_name" {
+  value = aws_eks_node_group.tf_eks_ng.node_group_name
+}
