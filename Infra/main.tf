8430816fb4784fd5a56319c9d7691846fa2520e6
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-08-16 16:52:22 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -7,13 +7,13 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.region
-}
-
 # Look up the EKS cluster to wire up k8s/helm providers
 data "aws_eks_cluster" "this" { name = var.cluster_name }
 data "aws_eks_cluster_auth" "this" { name = var.cluster_name }
+
+provider "aws" {
+  region = var.region
+}
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
