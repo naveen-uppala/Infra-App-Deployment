17f34067be448ade6a0361028dfa4549ff89fedf
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-08-16 17:19:00 +0530

    Update outputs.tf

--- Infra/modules/tf-eks/outputs.tf
+++ Infra/modules/tf-eks/outputs.tf
@@ -1,4 +1,4 @@
-/*
+
 output "tf_eks_cluster_name" {
   value = aws_eks_cluster.tf_eks_cluster.name
 }
@@ -12,4 +12,3 @@ output "tf_eks_node_group_name" {
   value = aws_eks_node_group.tf_eks_ng.node_group_name
 }
 
-*/
