48610aebdb3337c09c9767bf3998f909cce753cd
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-08-16 18:51:12 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -19,6 +19,7 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
+  depends_on = [aws_eks_cluster.this]
 }
 
 provider "helm" {
@@ -26,6 +27,7 @@ provider "helm" {
     host                   = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.this.token
+    depends_on = [aws_eks_cluster.this]
   }
 }
 
