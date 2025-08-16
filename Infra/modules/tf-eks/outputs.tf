ea43de02b4ca37fa75ce7da96d05e2bd5046615f
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-08-16 19:41:00 +0530

    Update outputs.tf

--- Infra/modules/tf-eks/outputs.tf
+++ Infra/modules/tf-eks/outputs.tf
@@ -1,4 +1,4 @@
-
+// modules/tf-eks/outputs.tf
 output "tf_eks_cluster_name" {
   value = aws_eks_cluster.tf_eks_cluster.name
 }
@@ -11,4 +11,10 @@ output "tf_eks_cluster_security_group_id" {
 output "tf_eks_node_group_name" {
   value = aws_eks_node_group.tf_eks_ng.node_group_name
 }
-
+# Used by root providers
+output "tf_eks_cluster_endpoint" {
+  value = aws_eks_cluster.tf_eks_cluster.endpoint
+}
+output "tf_eks_cluster_ca_data" {
+  value = aws_eks_cluster.tf_eks_cluster.certificate_authority[0].data
+}
