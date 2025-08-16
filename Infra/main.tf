ca9c9e1d6098833c53b6411a71493cf455cd329b
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-08-16 18:06:59 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">= 1.5.0"
   required_providers {
-    aws =         { source  = "hashicorp/aws", version = ">= 5.0" }
-    kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.27" }
-    helm  =      { source = "hashicorp/helm",       version = ">= 2.11.0" }
+    aws        = { source = "hashicorp/aws",       version = ">= 5.0" }
+    kubernetes = { source = "hashicorp/kubernetes",version = ">= 2.27" }
+    helm       = { source = "hashicorp/helm",      version = "~> 2.11.0" } # force v2
   }
 }
 
