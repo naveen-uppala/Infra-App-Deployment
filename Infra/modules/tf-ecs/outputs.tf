1e5a2078e6767b2c4294af0737310d7451c8887d
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-09-26 23:42:22 +0530

    Update outputs.tf

--- Infra/modules/tf-ecs/outputs.tf
+++ Infra/modules/tf-ecs/outputs.tf
@@ -1,5 +1,6 @@
-output "service_security_group_id" {
-  value = aws_security_group.ecs_service.id
+output "ecs_service_sg_id" {
+  description = "Security group used by ECS tasks"
+  value       = aws_security_group.ecs_service.id
 }
 
 output "subnet_ids" {
