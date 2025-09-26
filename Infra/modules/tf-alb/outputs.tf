3ca4af7f48250c1305ea6ba833da5537d869d9c9
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-09-27 00:03:31 +0530

    Update outputs.tf

--- Infra/modules/tf-alb/outputs.tf
+++ Infra/modules/tf-alb/outputs.tf
@@ -8,10 +8,11 @@ output "alb_dns_name" {
   value       = aws_lb.frontend_alb.dns_name
 }
 
-output "alb_sg_id" {
+output "alb_security_group_id" {
   description = "Security group ID of the frontend ALB"
   value       = aws_security_group.alb_sg.id
 }
+
 
 output "target_group_arn" {
   description = "Target group ARN"
