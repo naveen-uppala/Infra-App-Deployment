37037e1dd0bfc5e8669f2e75315e7a64077c13ac
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-09-26 17:45:18 +0530

    Update main.tf

--- Infra/modules/tf-rds/main.tf
+++ Infra/modules/tf-rds/main.tf
@@ -67,20 +67,3 @@ resource "aws_db_instance" "mysql" {
     Name = "mysql-db-instance"
   })
 }
-
-# ------------------ Outputs ------------------
-
-output "rds_endpoint" {
-  description = "RDS MySQL endpoint"
-  value       = aws_db_instance.mysql.endpoint
-}
-
-output "rds_port" {
-  description = "Port number for RDS"
-  value       = aws_db_instance.mysql.port
-}
-
-output "rds_security_group_id" {
-  description = "Security Group ID for RDS"
-  value       = aws_security_group.rds_mysql_sg.id
-}
