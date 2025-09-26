81369c884e15d8077aa3456a2cd02b1172fa102b
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-09-26 17:43:33 +0530

    Update variables.tf

--- Infra/modules/tf-rds/variables.tf
+++ Infra/modules/tf-rds/variables.tf
@@ -1,83 +1,35 @@
-variable "region" {
-  description = "The AWS region where resources will be created."
+variable "vpc_id" {
   type        = string
-  default     = "us-east-2" # Change this to your desired region
+  description = "VPC ID where the RDS will be created"
 }
 
-variable "db_instance_identifier" {
-  description = "The identifier for the RDS database instance."
-  type        = string
-  default     = "database-tcn"
+variable "data_subnet_ids" {
+  type        = list(string)
+  description = "List of data-tier subnet IDs for RDS subnet group"
 }
 
-variable "allocated_storage" {
-  description = "The allocated storage for the RDS database instance (in GB)."
-  type        = number
-  default     = 20
-}
-
-variable "storage_type" {
-  description = "The storage type for the RDS database instance."
-  type        = string
-  default     = "gp2"
-}
-
-variable "engine" {
-  description = "The database engine for the RDS database instance."
-  type        = string
-  default     = "mysql"
-}
-
-variable "engine_version" {
-  description = "The version of the database engine for the RDS database instance."
-  type        = string
-  default     = "8.0.33"
-}
-
-variable "instance_class" {
-  description = "The instance class for the RDS database instance."
-  type        = string
-  default     = "db.t3.micro"
-}
-
-variable "db_name" {
-  description = "The name of the database to be created."
-  type        = string
-  default     = "tcn"
+variable "tags" {
+  type        = map(string)
+  description = "Common tags"
 }
 
 variable "db_username" {
-  description = "The username for accessing the database."
   type        = string
-  default     = "tcn"
+  description = "Master username for the RDS instance"
 }
 
 variable "db_password" {
-  description = "The password for accessing the database."
   type        = string
-  default     = "tcntcntcn"
+  description = "Master password for the RDS instance"
+  sensitive   = true
 }
 
-variable "vpc_security_group_ids" {
-  description = "The list of security group IDs associated with the RDS instance."
-  type        = list(string)
-  default     = ["sg-06ac155edea432fbc"]
-}
-
-variable "availability_zone" {
-  description = "The availability zone for the RDS instance."
+variable "db_name" {
   type        = string
-  default     = "us-east-2b"
+  description = "Initial database name"
 }
 
-variable "skip_final_snapshot" {
-  description = "Whether to skip the final snapshot when deleting the RDS instance."
-  type        = bool
-  default     = true
-}
-
-variable "final_snapshot_identifier" {
-  description = "The identifier for the final snapshot when deleting the RDS instance."
+variable "db_instance_class" {
   type        = string
-  default     = "tcn-snapshot-rds"
+  default     = "db.t3.micro"
 }
