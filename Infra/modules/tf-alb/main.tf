fb1e3b0110c135a2ae077cbf11d92adda37784a8
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-09-26 22:49:40 +0530

    Update main.tf

--- Infra/modules/tf-alb/main.tf
+++ Infra/modules/tf-alb/main.tf
@@ -44,10 +44,10 @@ resource "aws_lb" "frontend_alb" {
 
 resource "aws_lb_target_group" "frontend_tg" {
   name        = "${var.alb_name}-tg"
-  port        = 80
+  port        = 5000
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
-  target_type = "instance"
+  target_type = "ip"
 
   health_check {
     enabled             = true
@@ -72,7 +72,7 @@ resource "aws_lb_listener" "https_listener" {
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.acm_certificate_arn   # 👈 We'll pass this from root module
+  certificate_arn   = var.acm_certificate_arn  
 
   default_action {
     type             = "forward"
