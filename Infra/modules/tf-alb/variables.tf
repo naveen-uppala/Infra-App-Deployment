b511921b852c8064e473462bead3ea3d60db4efd
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-09-26 20:37:56 +0530

    Update variables.tf

--- Infra/modules/tf-alb/variables.tf
+++ Infra/modules/tf-alb/variables.tf
@@ -1,11 +1,11 @@
 variable "vpc_id" {
   type        = string
-  description = "VPC ID where the ALB will be created"
+  description = "VPC ID"
 }
 
 variable "public_subnet_ids" {
   type        = list(string)
-  description = "List of public subnet IDs where the ALB will be deployed"
+  description = "List of public subnet IDs"
 }
 
 variable "tags" {
@@ -15,6 +15,11 @@ variable "tags" {
 
 variable "alb_name" {
   type        = string
+  description = "Name of the ALB"
   default     = "frontend-alb"
-  description = "Name of the Application Load Balancer"
+}
+
+variable "acm_certificate_arn" {
+  type        = string
+  description = "ARN of the ACM certificate for HTTPS listener"
 }
