51943cde71af6179811d152eb19fca842d0c5847
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-09-26 23:50:08 +0530

    Update main.tf

--- Infra/modules/tf-ecs/main.tf
+++ Infra/modules/tf-ecs/main.tf
@@ -39,12 +39,22 @@ resource "aws_security_group" "ecs_service" {
   description = "Security group for ECS services"
   vpc_id      = var.vpc_id
 
+  # Inbound: Allow only traffic from ALB SG
+  ingress {
+    description     = "Allow traffic from frontend ALB"
+    from_port       = 5000
+    to_port         = 5000
+    protocol        = "tcp"
+    security_groups = [var.alb_security_group_id]
+  }
+
+  # Egress: Allow all outbound
   egress {
-    description = "Allow all egress"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "Allow all egress"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = []
   }
 
