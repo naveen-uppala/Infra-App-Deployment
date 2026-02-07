5b2d4a3fb3e275945936fbfece62c8be02e29e81
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2026-02-07 23:44:20 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -57,7 +57,7 @@ locals {
 module "vpc_endpoints" {
   source = "./modules/tf-vpc-endpoints"
   vpc_id                   = module.vpc.vpc_id
-  private_route_table_ids  = values(module.vpc.private_route_table_id)
+  private_route_table_id  = values(module.vpc.private_route_table_id)
   private_subnet_ids       = values(module.vpc.private_subnet_ids)
   region                   = var.region
   vpc_cidr                 = var.vpc_cidr
