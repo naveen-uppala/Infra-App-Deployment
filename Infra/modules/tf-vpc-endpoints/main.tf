6d3e6cb885a779ee675b988b2d32ceb2cd30c2bf
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2026-02-07 23:04:19 +0530

    Create main.tf

---
+++ Infra/modules/tf-vpc-endpoints/main.tf
@@ -0,0 +1,66 @@
+
+resource "aws_security_group" "vpc_endpoints" {
+  name        = "vpc-endpoints-sg"
+  description = "Allow HTTPS from private subnets to VPC endpoints"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.vpc_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "vpc-endpoints-sg"
+  }
+}
+
+
+locals {
+  interface_endpoints = [
+    "eks",
+    "sts",
+    "ec2",
+    "ecr.api",
+    "ecr.dkr",
+    "logs"
+  ]
+}
+
+resource "aws_vpc_endpoint" "interface" {
+  for_each = toset(local.interface_endpoints)
+
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${var.region}.${each.key}"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = var.private_subnet_ids
+
+  security_group_ids  = [aws_security_group.vpc_endpoints.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "vpce-${each.key}"
+  }
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = var.private_route_table_ids
+
+  tags = {
+    Name = "vpce-s3"
+  }
+}
