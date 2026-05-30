b566eb966fb28ff771de18f87df1f1b749f4e6a4
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2026-05-30 23:28:09 +0530

    Update main.tf

--- Network/main.tf
+++ Network/main.tf
@@ -48,7 +48,7 @@ locals {
 module "vpc_endpoints" {
   source                 = "../modules/tf-vpc-endpoints"
   vpc_id                 = module.tf_vpc.vpc_id
-  private_route_table_id = values(module.tf_vpc.private_route_table_id)
+  private_route_table_id = module.tf_vpc.private_route_table_id
   private_subnet_ids     = local.app_tier_subnet_ids
   region                 = var.region
   vpc_cidr               = var.vpc_cidr
