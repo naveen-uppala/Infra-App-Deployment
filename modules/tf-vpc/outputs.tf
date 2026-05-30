07fad4680aac30cd94daa79601d372defc2217c9
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2026-05-30 23:22:44 +0530

    Update outputs.tf

--- modules/tf-vpc/outputs.tf
+++ modules/tf-vpc/outputs.tf
@@ -33,7 +33,12 @@ output "public_route_table_id" {
   value = aws_route_table.public.id
 }
 
+output "private_route_table_id" {
+  value = [aws_route_table.private.id]
+}
 
+/*
 output "private_route_table_id" {
   value = { for k, rt in aws_route_table.private : k => rt.id }
 }
+*/
